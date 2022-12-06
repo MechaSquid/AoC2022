9ad013807bcc83119dbfca954c3baea2ff74be51
MechaSquid <satanpuppet@gmail.com>
2022-12-06 13:56:16 -0500

    Update day2BROKEN.py

--- day2BROKEN.py
+++ day2BROKEN.py
@@ -4,40 +4,28 @@
 # lose = AZ BX CY 
 # X=1, y=2, z=3, win=6, tie=3, lose=0
 
+rps ={
+    "A X": 4,
+    "B Y": 5,
+    "C Z": 6,
+    "A Y": 8,
+    "B Z": 9,
+    "C X": 7,
+    "A Z": 3,
+    "B X": 1,
+    "C Y": 2,
+}
+
+print(rps)
+
 score = 0 
 
 with open ('input.txt', 'r') as f:
     lines = f.readlines()
-    print(score)
     
     for line in lines:
-        lon = str(line)
-        print(lon)
-        lon=lon.replace(" ", "")
-        print(lon)
-        
-        if lon == "AX":
-            score = score + 4
-        elif lon == "BY":
-            score += 5
-        elif lon == "CZ":
-            score += 6
-        elif lon == "AY":
-            score = score + 8
-            print(score)
-        elif lon == "BZ":
-            score = score + 9
-            print(score)
-        elif lon == "CX":
-            score += 7
-            print(score)
-        elif lon == "AZ":
-            score += 3 
-        elif lon == "BX":
-            score += 1
-        elif lon == "CY":
-            score += 2
-        else:
-            print("womp")
-    
+        print("womp")
+
 print(score)
+
+##Notes: set up dictionary, create with key value like AX:5, hey, score = dictionary + ax, pull int value you want from the key of the ax etc
